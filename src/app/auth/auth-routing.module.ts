84dd1f5f73f37fb721b5b43c4df6004e335db1bf
Shaimaa-Mady <shaimaa.mady99@gmail.com>
2025-08-03 04:50:37 +0300

    feature/auth: create all needed components & handle main auth layout with design & handle auth routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, title: 'login' },
+      { path: 'register', component: RegisterComponent, title: 'register' },
+      {
+        path: 'forget-password',
+        component: ForgetPasswordComponent,
+        title: 'forget-password',
+      },
+      {
+        path: 'reset-password',
+        component: ResetPasswordComponent,
+        title: 'reset-password',
+      },
+      {
+        path: 'change-password',
+        component: ChangePasswordComponent,
+        title: 'change-password',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { AuthComponent } from './auth.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ForgetPasswordComponent,
+    ResetPasswordComponent,
+    ChangePasswordComponent,
+    AuthComponent
+  ],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}

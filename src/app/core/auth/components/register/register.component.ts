5990947e111c753aca38e53daf420cd4b957b202
Shaimaa-Mady <shaimaa.mady99@gmail.com>
2025-08-05 21:59:26 +0300

    feature/register: finished register page design

--- src/app/core/auth/components/register/register.component.ts
+++ src/app/core/auth/components/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { LoginResponse } from '../../interface/login-response';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+   isHide: boolean = true;
+    userToken: string = '';
+  
+    constructor(private _AuthService: AuthService, private _Router:Router) {}
+  
+    loginForm: FormGroup = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  
+    onRegister() {
+      this._AuthService.login(this.loginForm.value).subscribe({
+        next: (resp: LoginResponse) => {
+          console.log(resp);
+          this.userToken = resp.token;
+          localStorage.setItem('userToken', this.userToken);
+          this._AuthService.tokenDecode(this.userToken);
+          this._Router.navigate(['/home'])
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err);
+        },
+      });
+    }
 
 }

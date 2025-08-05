1bed61ac27e84cfbac4d15c1f6c0167b19d8d6c4
Shaimaa-Mady <shaimaa.mady99@gmail.com>
2025-08-05 20:44:58 +0300

    bugfix/auth-architecture: create not found and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthModule } from './core/auth/auth.module';
+import { DashboaredModule } from './dashboared/dashboared.module';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
+    DashboaredModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

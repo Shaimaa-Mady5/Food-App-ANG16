8debed40b52c81acab66e7452ec4e01c8b092ff8
Shaimaa-Mady <shaimaa.mady99@gmail.com>
2025-08-09 16:25:29 +0300

    feature/verify-account: designed all verifu-account with add ts code and verify interface and handle toaster

--- src/app/core/auth/components/verify/verify.component.ts
+++ src/app/core/auth/components/verify/verify.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginResponse } from '../../interface/login-response';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RegisterResponse } from '../../interface/register-response';
 
 @Component({
   selector: 'app-verify',
   templateUrl: './verify.component.html',
-  styleUrls: ['./verify.component.scss']
+  styleUrls: ['./verify.component.scss'],
 })
 export class VerifyComponent {
+  errorMsg: string = ';';
+  successMsg: string = ';';
 
+  constructor(
+    private _AuthService: AuthService,
+    private _Router: Router,
+    private _ToastrService: ToastrService
+  ) {}
+  verifyForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    code: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(6),
+    ]),
+  });
+
+  onVerify() {
+    if (this.verifyForm.valid) {
+      this._AuthService.verifyAccount(this.verifyForm.value).subscribe({
+        next: (resp: RegisterResponse) => {
+          console.log(resp);
+          this.successMsg = resp.message;
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err);
+          this.errorMsg = err.error.message;
+          this.showError();
+        },
+        complete: () => {
+          this._Router.navigate(['/login']);
+        },
+      });
+    } else {
+      this.verifyForm.markAllAsTouched();
+    }
+  }
+  showError() {
+    this._ToastrService.error(this.errorMsg);
+  }
+  showSuccess() {
+    this._ToastrService.success(this.successMsg);
+  }
 }
--- src/app/core/auth/service/auth.service.ts
+++ src/app/core/auth/service/auth.service.ts
@@ -7,6 +7,7 @@ import { jwtDecode } from 'jwt-decode';
 import { RegisterResponse } from '../interface/register-response';
 import { RegisterUser } from '../interface/register-user';
 import { ResetDetails } from '../interface/reset-details';
+import { VerifyAccount } from '../interface/verify-account';
 
 @Injectable({
   providedIn: 'root',
@@ -23,30 +24,48 @@ export class AuthService {
   }
   // end:: JWT
 
-  // start::login 
+  // start::register
+  register(user: RegisterUser): Observable<RegisterResponse> {
+    return this._HttpClient.post<RegisterResponse>(
+      this.mainUrl + 'users/register',
+      user
+    );
+  }
+  // end::register
+
+  // start::verify account
+  verifyAccount(userDetails: VerifyAccount): Observable<RegisterResponse> {
+    return this._HttpClient.put<RegisterResponse>(
+      this.mainUrl + 'Users/verify',
+      userDetails
+    );
+  }
+  // end::verify account
+
+  // start::login
   login(user: LoginUser): Observable<LoginResponse> {
     return this._HttpClient.post<LoginResponse>(
       this.mainUrl + 'Users/Login',
       user
     );
   }
-  // end::login 
-
-  // start::register
-  register(user:RegisterUser):Observable<RegisterResponse>{
-   return this._HttpClient.post<RegisterResponse>(this.mainUrl+'users/register',user)
-  }
-  // end::register
+  // end::login
 
   // start::forgetPassword
-  forgetPassword(userEmail:string) :Observable<RegisterResponse>{
-   return this._HttpClient.post<RegisterResponse>(this.mainUrl+'Users/Reset/Request',userEmail)
+  forgetPassword(userEmail: string): Observable<RegisterResponse> {
+    return this._HttpClient.post<RegisterResponse>(
+      this.mainUrl + 'Users/Reset/Request',
+      userEmail
+    );
   }
   // end::forgetPassword
 
-    // start::forgetPassword
-  resetPassword(userEmail:ResetDetails) :Observable<RegisterResponse>{
-   return this._HttpClient.post<RegisterResponse>(this.mainUrl+'Users/Reset',userEmail)
+  // start::resetPassword
+  resetPassword(userDetails: ResetDetails): Observable<RegisterResponse> {
+    return this._HttpClient.post<RegisterResponse>(
+      this.mainUrl + 'Users/Reset',
+      userDetails
+    );
   }
-  // end::forgetPassword
+  // end::resetPassword
 }
